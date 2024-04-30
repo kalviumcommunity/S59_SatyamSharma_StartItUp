90431590c4248a2194b96f10400c5d9e5c5949e0
Satyam <satyamworks20@gmail.com>
2024-04-30 10:21:06 +0530

    Some More Changes Are Been Made

--- client/src/Pages/InvestmentDetails.jsx
+++ client/src/Pages/InvestmentDetails.jsx
@@ -70,7 +70,7 @@ function InvestmentDetails() {
     </div>
     <button
       class="mt-6 block w-full select-none rounded-lg bg-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-      type="button">
+      type="submit">
       Submit
     </button>
   </form>
